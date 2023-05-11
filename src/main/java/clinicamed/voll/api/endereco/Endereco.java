0a1df3c01a6f03d4a26248597ece36ae0f1a07f4
Diego da Silva <diegosantanasantos@yahoo.com.br>
2023-05-11 18:25:31 -0300

    Finalizando JPA

--- src/main/java/clinicamed/voll/api/endereco/Endereco.java
+++ src/main/java/clinicamed/voll/api/endereco/Endereco.java
@@ -2,8 +2,16 @@ package clinicamed.voll.api.endereco;
 
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Embedded;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Endereco {
     private String logradouro;
     private  String bairro;
--- src/main/java/clinicamed/voll/api/medico/Medico.java
+++ src/main/java/clinicamed/voll/api/medico/Medico.java
@@ -2,9 +2,19 @@ package clinicamed.voll.api.medico;
 
 import clinicamed.voll.api.endereco.Endereco;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Table(name = "medicos")
 @Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+
 public class Medico {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
